2d6230b14410fef516af90c84c12a0ba1ce8f5f8
shem958 <shemgikunda37@gmail.com>
2024-11-05 14:54:13 +0300

    "Updated Footer component: added margin top to social media links container, added 'Back to Top' link"

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-gray-800 p-4 mt-8">
       <div className="container mx-auto text-center">
         <p className="text-white">© {new Date().getFullYear()} My Portfolio</p>
-        <div className="flex justify-center space-x-4">
+        <div className="flex justify-center space-x-4 mt-2">
           <a
             href="https://twitter.com"
             target="_blank"
@@ -29,6 +29,9 @@ const Footer = () => {
             LinkedIn
           </a>
         </div>
+        <a href="#top" className="text-white hover:text-gray-400 mt-4 block">
+          Back to Top
+        </a>
       </div>
     </footer>
   );
